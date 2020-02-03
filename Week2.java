49945f7ae726cbc5c4f69d7f118ba1499c62cab0
Felix Quinones <60441061+Felix-36@users.noreply.github.com>
2020-02-03 12:35:47 -0500

    Update Week2.java

--- Week2.java
+++ Week2.java
@@ -1,4 +1,26 @@
-
+/**
+ * 
+ * @author Felix
+ * Created 2.3.20
+ *
+ */
 public class Week2 {
+	public static void main(String[] args) {
+		// Define Variables
+		double size =10.35;
+		byte space =5;
+		int age =3;
+		
+		//Declaring Variables
+		String name1;
+		String name2;
+		String name3;
+		
+		//Assign Values
+		name1= "Paul";
+		name2= "Jen";
+		name3= "Ally";
+		
+	}
 
 }
